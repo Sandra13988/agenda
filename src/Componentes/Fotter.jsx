faff8d4bf9068a25f02c47f32b46cec9e4e8a171
Sandra13988 <sandra13988@gmail.com>
2024-05-09 16:40:52 +0200

    Añadido redes sociales

--- src/Componentes/Fotter.jsx
+++ src/Componentes/Fotter.jsx
@@ -8,9 +8,21 @@ export const Fotter = () => {
     return (
         <>
 
-            <div><img src="https://cdn.hugeicons.com/icons/linkedin-02-stroke-rounded.svg" alt="linkedin-02" width="24" height="24" /></div>
-            <div><img src="https://cdn.hugeicons.com/icons/instagram-stroke-rounded.svg" alt="instagram" width="24" height="24" /></div>
-            <div><img src="https://cdn.hugeicons.com/icons/twitter-stroke-rounded.svg" alt="twitter" width="24" height="24" /></div>
+            <div>
+                <a href="https://www.linkedin.com/in/sandra-rubio-sanchez-7233921a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
+                    <img src="https://cdn.hugeicons.com/icons/linkedin-02-stroke-rounded.svg" alt="linkedin-02" width="35" height="35" />
+                </a>
+            </div>
+            <div>
+                <a href="https://www.instagram.com/sandra_srsi?igsh=MW1yYTY0ajRsZ25o" target="_blank" rel="noopener noreferrer">
+                    <img src="https://cdn.hugeicons.com/icons/instagram-stroke-rounded.svg" alt="instagram" width="35" height="35" />
+                </a>
+            </div>
+            <div>
+                <a href="https://x.com/Deskiziada88?t=JtnwgySJ7yO5f036cBMXYw&s=09" target="_blank" rel="noopener noreferrer">
+                    <img src="https://cdn.hugeicons.com/icons/twitter-stroke-rounded.svg" alt="twitter" width="35" height="35" />
+                </a>
+            </div>
 
         </>
     )
